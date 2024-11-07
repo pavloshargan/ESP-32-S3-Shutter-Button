c457d298f48b39676645d192d80420cf1ab43bff
Pavlo Sharhan <pavel.shargan@gmail.com>
2024-11-06 20:00:11 -0500

    Update KeyboardMouseExample.ino
    added volume up on button

--- examples/KeyboardMouseExample/KeyboardMouseExample.ino
+++ examples/KeyboardMouseExample/KeyboardMouseExample.ino
@@ -1,109 +1,95 @@
-/**
- * This example turns the ESP32 into a Bluetooth LE keyboard and mouse that presses Tab and Enter every second
- 
 #include <BleCombo.h>
+
+const int buttonPin = 0;  // the number of the pushbutton pin
+int buttonState = 0;
+int curState = 0;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting work!");
   Keyboard.begin();
   Mouse.begin();
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  if(Keyboard.isConnected()) {
-    // Serial.println("Sending 'Hello world'");
-    // Keyboard.println("Hello World");
+  curState = digitalRead(buttonPin);
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (curState == LOW && buttonState!=LOW) {
+    if (Keyboard.isConnected()) {
+      Keyboard.write(KEY_MEDIA_VOLUME_DOWN);
+    }
+    Serial.println("Button pressed");
+  } 
+  buttonState = curState;
 
-    // delay(1000);
-    Serial.println("Sending Tab key...");
-    Keyboard.write(KEY_TAB);
+  // Check if there is data available on the serial port
+  if (Serial.available() > 0) {
+    // Read the input from serial
+    String input = Serial.readStringUntil('\n');
+    input.trim(); // Remove any leading/trailing whitespace
 
-    delay(1000);
-
-    Serial.println("Sending Enter key...");
-    Keyboard.write(KEY_RETURN);
-
-    delay(1000);
-
-
-  
-  
-    // Serial.println("Sending Play/Pause media key...");
-    // Keyboard.write(KEY_MEDIA_PLAY_PAUSE);
-
-    // delay(1000);
-
-    // Serial.println("Sending Ctrl+Alt+Delete...");
-    // Keyboard.press(KEY_LEFT_CTRL);
-    // Keyboard.press(KEY_LEFT_ALT);
-    // Keyboard.press(KEY_DELETE);
-    // delay(100);
-    // Keyboard.releaseAll();
-
-    // unsigned long startTime;
-
-    // Serial.println("Move mouse pointer up");
-    // startTime = millis();
-    // while(millis()<startTime+1000) {
-    //   Mouse.move(0,-1);
-    //   delay(5);
-    // }
-    // Serial.println("Move mouse pointer left");
-    // startTime = millis();
-    // while(millis()<startTime+1000) {
-    //   Mouse.move(-1,0);
-    //   delay(5);
-    // }
-
-    // Serial.println("Move mouse pointer down");
-    // startTime = millis();
-    // while(millis()<startTime+1000) {
-    //   Mouse.move(0,1);
-    //   delay(5);
-    // }
-
-    // Serial.println("Move mouse pointer right");
-    // startTime = millis();
-    // while(millis()<startTime+1000) {
-    //   Mouse.move(1,0);
-    //   delay(5);
-    // }
-    
-    // Serial.println("Scroll Down");
-    // Mouse.move(0,0,-1);
-
-    // Serial.println("Left click");
-    // Mouse.click(MOUSE_LEFT);
-    // delay(500);
-
-    // Serial.println("Right click");
-    // Mouse.click(MOUSE_RIGHT);
-    // delay(500);
-
-    // Serial.println("Scroll wheel click");
-    // Mouse.click(MOUSE_MIDDLE);
-    // delay(500);
-
-    // Serial.println("Back button click");
-    // Mouse.click(MOUSE_BACK);
-    // delay(500);
-
-    // Serial.println("Forward button click");
-    // Mouse.click(MOUSE_FORWARD);
-    // delay(500);
-
-    // Serial.println("Click left+right mouse button at the same time");
-    // Mouse.click(MOUSE_LEFT | MOUSE_RIGHT);
-    // delay(500);
-
-    // Serial.println("Click left+right mouse button and scroll wheel at the same time");
-    // Mouse.click(MOUSE_LEFT | MOUSE_RIGHT | MOUSE_MIDDLE);
-    // delay(500);
-
-
+    // Perform actions based on the input command
+    if (input.equalsIgnoreCase("click")) {
+      Serial.println("Received 'click' command. Sending left mouse click...");
+      if (Keyboard.isConnected()) {
+        Mouse.click(MOUSE_LEFT);
+      }
+    } 
+    else if (input.equalsIgnoreCase("down")) {
+      Serial.println("Received 'down' command. Sending down...");
+      if (Keyboard.isConnected()) {
+        Keyboard.write(KEY_MEDIA_VOLUME_DOWN);
+      }
+    } 
+    else if (input.equalsIgnoreCase("up")) {
+      Serial.println("Received 'up' command. Sending up key...");
+      if (Keyboard.isConnected()) {
+        Keyboard.write(KEY_MEDIA_VOLUME_DOWN);
+      }
+    } 
+    else if (input.equalsIgnoreCase("toggle")) {
+      Serial.println("Received 'toggle' command. Sending Toggle key...");
+      if (Keyboard.isConnected()) {
+        Keyboard.write(KEY_MEDIA_PLAY_PAUSE);
+      }
+    } 
+    else if (input.equalsIgnoreCase("tab")) {
+      Serial.println("Received 'tab' command. Sending Tab key...");
+      if (Keyboard.isConnected()) {
+        Keyboard.write(KEY_TAB);
+      }
+    } 
+    else if (input.equalsIgnoreCase("enter")) {
+      Serial.println("Received 'enter' command. Sending Enter key...");
+      if (Keyboard.isConnected()) {
+        Keyboard.write(KEY_RETURN);
+      }
+    }
+    // New mouse movement commands
+    else if (input.equalsIgnoreCase("up")) {
+      Serial.println("Received 'up' command. Moving mouse up...");
+      if (Keyboard.isConnected()) {
+        Mouse.move(0, -20);  // Move up by 20 pixels
+      }
+    } 
+    else if (input.equalsIgnoreCase("down")) {
+      Serial.println("Received 'down' command. Moving mouse down...");
+      if (Keyboard.isConnected()) {
+        Mouse.move(0, 20);  // Move down by 20 pixels
+      }
+    } 
+    else if (input.equalsIgnoreCase("left")) {
+      Serial.println("Received 'left' command. Moving mouse left...");
+      if (Keyboard.isConnected()) {
+        Mouse.move(-20, 0);  // Move left by 20 pixels
+      }
+    } 
+    else if (input.equalsIgnoreCase("right")) {
+      Serial.println("Received 'right' command. Moving mouse right...");
+      if (Keyboard.isConnected()) {
+        Mouse.move(20, 0);  // Move right by 20 pixels
+      }
+    }
   }
-  
-  Serial.println("Waiting 2 seconds...");
-  delay(2000);
 }
